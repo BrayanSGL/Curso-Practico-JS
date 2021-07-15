93a51944864937bb8cbb4b533b0c1f4a56dd7ddf
Brayan Snader Galeano Lara <bgaleanolara@gmail.com>
2021-07-14 21:11:44 -0500

    Continuamos el tercer taller

---
+++ Taller #3/Taller/moda.js
@@ -0,0 +1,20 @@
+const lista1 = [1, 2, 3, 1, 3, 3, 3, 3, 1, 3, 2, 2, 1, 2, 3];
+
+const lista1Count = {};
+
+lista1.map(function (elemento) {
+  if (lista1Count[elemento]) {
+    lista1Count[elemento] += 1;
+  } else {
+    lista1Count[elemento] = 1;
+  }
+});
+
+const lista1Array = Object.entries(lista1Count).sort(function (
+  valorAcomulado,
+  nuevoValor
+) {
+  return valorAcomulado[1] - nuevoValor[1];
+});
+
+const moda = lista1Array[lista1Array.length - 1];
